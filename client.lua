771526aee01261ea27079cf83f2a483edee22ee6
Sun <30409932+Origintk@users.noreply.github.com>
2022-01-19 21:07:52 -0300

    eng

--- client.lua
+++ client.lua
@@ -1,6 +1,6 @@
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(22*60000)
+		Citizen.Wait(22*60000) -- example 22 minutos /22 minutes 
 		TriggerServerEvent("redm:paycheck")
 	end
 end)
