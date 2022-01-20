8311c8a04e573a04e0d269983e1e395f6a7230f8
Sun <30409932+Origintk@users.noreply.github.com>
2022-01-19 21:02:01 -0300

    Up

---
+++ server.lua
@@ -0,0 +1,39 @@
+VORP = exports.vorp_inventory:vorp_inventoryApi()
+
+local data = {}
+
+Citizen.CreateThread(function()
+    data = exports.vorp_inventory:vorp_inventoryApi()
+end) 
+
+local VorpCore = {}
+
+TriggerEvent("getCore",function(core)
+    VorpCore = core
+end)
+
+
+RegisterNetEvent("redm:paycheck")
+AddEventHandler("redm:paycheck",function()
+	local _source = source
+	local User = VorpCore.getUser(_source)
+	local Character = User.getUsedCharacter
+	local group = Character.group
+
+		if group == "admin" then -- grupo qual irá receber o pagamento
+		Character.addCurrency(0, 100)
+		TriggerClientEvent('vorp:TipRight',_source, "Você recebeu seu salário")
+		end
+		
+		if group == "admin" then -- grupo qual irá receber o pagamento
+        Character.addCurrency(0, 100)
+		TriggerClientEvent('vorp:TipRight',_source, "Você recebeu seu salário")
+		end
+		--exemplo
+		--if group == "admin" then -- grupo
+       -- Character.addCurrency(0, 100)
+		--end
+		
+		
+	end
+end)
